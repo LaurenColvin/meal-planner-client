1ec05da311ce255904eed0b2df756d7b37822d59
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-03-18 22:39:07 -0700

    creating new component to add ingredients to recipes

---
+++ src/AddIngredients/AddIngredients.js
@@ -0,0 +1,9 @@
+const AddIngredients = () => {
+    return (
+      <div className="add-ingredients-container">
+        <h1>Add Your Ingredients</h1>
+      </div>
+    );
+  };
+  
+  export default AddIngredients;
