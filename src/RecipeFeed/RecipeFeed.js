018324a539f0c0ddc212dda056576af1b1c1aa98
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-03-21 15:19:55 -0700

    importing Modal and bootstrap

--- src/RecipeFeed/RecipeFeed.js
+++ src/RecipeFeed/RecipeFeed.js
@@ -1,125 +1,140 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { Modal, Button } from "react-bootstrap";
+import ModalComponent from "../Modal/ModalComponent";
 
 const RecipeFeed = (props) => {
+  const [recipes, setRecipes] = useState([]);
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+  const [imgUrl, setImgUrl] = useState("");
+  const [show, setShow] = useState(false);
 
-    const [recipes, setRecipes] = useState([]);
-    const [title, setTitle] = useState("");
-    const [url, setUrl] = useState("");
-    const [imgUrl, setImgUrl] = useState("");
-  
-    const handleClick = (event) => {
-      event.preventDefault();
-      fetch(props.urlBase + "/recipe")
-        .then((response) => response.json())
-        .then((data) => setRecipes(data.recipe))
-        .catch(() => console.log("oops, error"));
-    };
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      console.log(title);
-      let data = {
-        title: title,
-        url: url,
-        imgUrl: imgUrl,
-      };
-      let options = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      };
-      fetch(props.urlBase + "/recipe", options)
-        .then((res) => res.json())
-        .then((d) => console.log(d))
-        .then(() => setTitle(""))
-        .then(() => setUrl(""))
-        .then(() => setImgUrl(""));
-    };
-  
-    const titleHandleChange = (event) => {
-      event.preventDefault();
-      setTitle(event.target.value);
-    };
-  
-    const urlHandleChange = (event) => {
-      event.preventDefault();
-      setUrl(event.target.value);
-    };
-  
-    const imgUrlHandleChange = (event) => {
-      event.preventDefault();
-      setImgUrl(event.target.value);
-    };
-  
-    const handleDelete = (event) => {
-      event.preventDefault();
-      let id = event.target.id;
-      let url = props.urlBase + "/recipe/" + id;
-      fetch(url, { method: "DELETE" })
-        .then((res) => res.json())
-        .then((d) => console.log(d));
-    };
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-    // const handleUpdate = (event) => {
-    //     event.preventDefault();
-    //     props.setRecipeId(event.target.id);
-    //   };
-  
-    const list = recipes.map((recipe, index) => {
-      return (
-        <div className="recipe-card" key={index}>
-          <a
-            className="link"
-            href={recipe.url}
-            alt={recipe.title}
-            target="_blank"
-          >
-            <h2>{recipe.title}</h2>
-          </a>
-          <a
-            className="link"
-            href={recipe.url}
-            alt={recipe.title}
-            target="_blank"
-          >
-            <img
-              className="recipe-img"
-              src={recipe.imgUrl}
-              alt={recipe.title}
-            ></img>
-          </a>
-          <Link className="button-link" to="/add-ingredients">
-            <button id={recipe._id}>Add Ingredients</button>
-          </Link>
-          <button id={recipe._id} onClick={handleDelete}>
-            Delete
-          </button>
-        </div>
-      );
-    });
+  const handleClick = (event) => {
+    event.preventDefault();
+    fetch(props.urlBase + "/recipe")
+      .then((response) => response.json())
+      .then((data) => setRecipes(data.recipe))
+      .catch(() => console.log("oops, error"));
+  };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(title);
+    let data = {
+      title: title,
+      url: url,
+      imgUrl: imgUrl,
+    };
+    let options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+    fetch(props.urlBase + "/recipe", options)
+      .then((res) => res.json())
+      .then((d) => console.log(d))
+      .then(() => setTitle(""))
+      .then(() => setUrl(""))
+      .then(() => setImgUrl(""));
+  };
 
+  const titleHandleChange = (event) => {
+    event.preventDefault();
+    setTitle(event.target.value);
+  };
+
+  const urlHandleChange = (event) => {
+    event.preventDefault();
+    setUrl(event.target.value);
+  };
+
+  const imgUrlHandleChange = (event) => {
+    event.preventDefault();
+    setImgUrl(event.target.value);
+  };
+
+  const handleDelete = (event) => {
+    event.preventDefault();
+    let id = event.target.id;
+    let url = props.urlBase + "/recipe/" + id;
+    fetch(url, { method: "DELETE" })
+      .then((res) => res.json())
+      .then((d) => console.log(d));
+  };
+
+  // const handleUpdate = (event) => {
+  //     event.preventDefault();
+  //     props.setRecipeId(event.target.id);
+  //   };
+
+  const list = recipes.map((recipe, index) => {
     return (
-      <div className="recipe-feed">
-        <h2>
+      <div className="recipe-card" key={index}>
+        <a
+          className="link"
+          href={recipe.url}
+          alt={recipe.title}
+          target="_blank"
+        >
+          <h2>{recipe.title}</h2>
+        </a>
+        <a
+          className="link"
+          href={recipe.url}
+          alt={recipe.title}
+          target="_blank"
+        >
+          <img
+            className="recipe-img"
+            src={recipe.imgUrl}
+            alt={recipe.title}
+          ></img>
+        </a>
+        <button onClick={handleShow}>Modal</button>
+        <Link className="button-link" to="/add-ingredients">
+          <button id={recipe._id}>Add Ingredients</button>
+        </Link>
+        <button id={recipe._id} onClick={handleDelete}>
+          Delete
+        </button>
+      </div>
+    );
+  });
+
+  return (
+    <div className="recipe-feed">
+      <h2>
         Welcome to your new meal planner. Add your favorite recipe to get
         started!
       </h2>
       <form onSubmit={handleSubmit}>
         <label>
           Recipe Title:
-          <input onChange={titleHandleChange} type="text" value={title} required/>
+          <input
+            onChange={titleHandleChange}
+            type="text"
+            value={title}
+            required
+          />
         </label>
         <label>
           URL:
-          <input onChange={urlHandleChange} type="text" value={url} required/>
+          <input onChange={urlHandleChange} type="text" value={url} required />
         </label>
         <label>
           Image URL:
-          <input onChange={imgUrlHandleChange} type="text" value={imgUrl} required/>
+          <input
+            onChange={imgUrlHandleChange}
+            type="text"
+            value={imgUrl}
+            required
+          />
         </label>
         {/* <label>Ingredients:
           <input onChange={albumHandleChange} type='text' />
@@ -130,8 +145,11 @@ const RecipeFeed = (props) => {
         Get Recipes
       </button>
       <div className="recipes-container">{list}</div>
-      </div>
-    );
-  };
-  
-  export default RecipeFeed;
+      <Modal show={show} onHide={handleClose}>
+        <ModalComponent handleClose={handleClose}/>
+      </Modal>
+    </div>
+  );
+};
+
+export default RecipeFeed;
