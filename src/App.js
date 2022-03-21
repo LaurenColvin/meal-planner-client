32c1243498c65317cf1d1b8703bfaea5ffe040e6
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-03-21 08:27:23 -0700

    adding recipe ID prop to App component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import RecipeFeed from "./RecipeFeed/RecipeFeed";
 import AddIngredients from "./AddIngredients/AddIngredients";
@@ -8,14 +9,15 @@ let urlBase = "http://localhost:3000";
 
 function App() {
 
+  const [recipeId, setRecipeId] = useState("");
 
   return (
     <div className="App">
       <Link to='/recipe-feed'><h1>Meal Planner</h1></Link>
       <ScrollTop>
         <Routes>
-          <Route path="/recipe-feed" element={<RecipeFeed urlBase={urlBase}/>}/>
-          <Route path="/add-ingredients" element={<AddIngredients/>}/>
+          <Route path="/recipe-feed" element={<RecipeFeed urlBase={urlBase} setRecipeId={setRecipeId}/>}/>
+          <Route path="/add-ingredients" element={<AddIngredients urlBase={urlBase} recipeId={recipeId}/>}/>
         </Routes>
       </ScrollTop>
     </div>
